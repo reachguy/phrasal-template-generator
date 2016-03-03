4032be9b7846e854203fd1be7ddaaa0bd6cd46c3
Shogo Shimizu <shogoluc@gmail.com>
2016-03-03 11:21:00 -0500

    added periods between document and write

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ var verb = prompt('A past tense verb (ran, burped, etc.)')
 var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\n\nEnter the letter below:')
 
 if(story = 'a') {
-  document write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature verbed + 'for hours.')
+  document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature verbed + 'for hours.')
 }
 
 if(story = 'b') {
-  document write('Ameilia' + verbed + 'through the' + adjective + + colour + 'nebula escaping the space' + creature + .)
+  document.write('Ameilia' + verbed + 'through the' + adjective + + colour + 'nebula escaping the space' + creature + .)
 }
 
 if(story = 'c') {
-  document write('Jackson chose his' + adjective + , + colour + + creature + 'card and' + verbed + 'it to the table knowing he won.')
+  document.write('Jackson chose his' + adjective + , + colour + + creature + 'card and' + verbed + 'it to the table knowing he won.')
 }
 
