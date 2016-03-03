a870f0517f8c94784c2fe5ee7f8f3dc13e06507f
Shogo Shimizu <shogoluc@gmail.com>
2016-03-03 11:04:02 -0500

    added content

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var intro = alert('Let’s make a story together!');
+
+var color = prompt('A colour (orange, indigo, etc.)')
+
+var creature = prompt('A creature (dragon, moth, etc.)')
+
+var adjective = prompt('An adjective (beautiful, super, etc.)')
+
+var verb = prompt('A past tense verb (ran, burped, etc.)')
+
+var story = prompt('Which story would you like?\na) Snacking\nb) Escaping\nc) Winning\n\nEnter the letter below:')
+
+if(story = 'a') {
+  document write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature verbed + 'for hours.')
+}
+
+if(story = 'b') {
+  document write('Ameilia' + verbed + 'through the' + adjective + + colour + 'nebula escaping the space' + creature + .)
+}
+
+if(story = 'c') {
+  document write('Jackson chose his' + adjective + , + colour + + creature + 'card and' + verbed + 'it to the table knowing he won.')
+}
+
